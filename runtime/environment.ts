78d1b5a94b211119f746b34e01361375657727dd
ishan211 <34925387+ishan211@users.noreply.github.com>
2024-08-06 20:06:22 -0700

    Update environment.ts

--- runtime/environment.ts
+++ runtime/environment.ts
@@ -1,25 +1,51 @@
-import { RuntimeVal } from "./values.ts";
+import { MK_BOOL, MK_NULL, RuntimeVal } from "./values.ts";
+
+export function createGlobalEnv() {
+  const env = new Environment();
+  // Create Default Global Enviornment
+  env.declareVar("true", MK_BOOL(true), true);
+  env.declareVar("false", MK_BOOL(false), true);
+  env.declareVar("null", MK_NULL(), true);
+
+  return env;
+}
 
 export default class Environment {
   private parent?: Environment;
   private variables: Map<string, RuntimeVal>;
+  private constants: Set<string>;
 
   constructor(parentENV?: Environment) {
+    const global = parentENV ? true : false;
     this.parent = parentENV;
     this.variables = new Map();
+    this.constants = new Set();
   }
 
-  public declareVar(varname: string, value: RuntimeVal): RuntimeVal {
+  public declareVar(
+    varname: string,
+    value: RuntimeVal,
+    constant: boolean,
+  ): RuntimeVal {
     if (this.variables.has(varname)) {
       throw `Cannot declare variable ${varname}. As it already is defined.`;
     }
 
     this.variables.set(varname, value);
+    if (constant) {
+      this.constants.add(varname);
+    }
     return value;
   }
 
   public assignVar(varname: string, value: RuntimeVal): RuntimeVal {
     const env = this.resolve(varname);
+
+    // Cannot assign to constant
+    if (env.constants.has(varname)) {
+      throw `Cannot reasign to variable ${varname} as it was declared constant.`;
+    }
+
     env.variables.set(varname, value);
     return value;
   }
